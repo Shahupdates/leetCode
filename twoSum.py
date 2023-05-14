6189d71396ba21bdf731e30afdff27603a56d6ff
Shahupdates <120000782+Shahupdates@users.noreply.github.com>
2023-05-13 20:46:51 -0400

    Update twoSum.py

--- twoSum.py
+++ twoSum.py
@@ -1,22 +1,22 @@
-"""
-To solve this problem we can use a hash table to keep track of the indices of the numbers we have seen so far.
-We can iterate through the array and for each element "num", we check if the difference (target-num) exists
-in the hash table, if it does, we found the pair of numbers that add up to "target" and we return their indices..
-If it doesnt', we add 'num and its index to the hash table and continue iterating
-"""
-
-
-def twoSum(self, nums : List[int], target: int) -> List[int]:
-  seen = {}
-  # we initialize an empty hash table called seen, we use the enumerate function
-  # to iterate through the array "nums" along with its indices
-  for i, num in enumerate(nums):
-  # for each element num and index i, we calcualte the difference
-    complement = target - num
-    # we check if complement is already in the seen hash table, if it is were done
-    if complement in seen:
-      return [seen[complement], i] #[value, index]
-    # if complement is not in the seen hash table then we add num and its index i to sean and continue iterating
-    seen[num] = i
-  return [] # if we iterate through entire array and didnt find pair of numbers that add up to target
-    
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        /*
+        twoSum aims to find two numbers in a list that ends up to a given target value
+        This pattern optimizes the search process by trading space complexity for time complexity, by storing previously seen numbers and their indices in a hash table, you can check if the complement exists in constant time so the complexity is reduced from O(n^2) (nested loop approach) to O(n) (single loop approach)
+        Classic example of using a hash table to efficiently solve a problem that involves finding pairs of elements with a specific property (in this case adding up to a target value)
+        */
+        #initializes an empty dictionary named seen to keep track of previously seen numbers and their indices
+        seen = {}
+        #loop iterates over the elements of the nums list (num) along with their indices (i)
+        for i, num in enumerate(nums):
+            #the varialbe complement is calculated as the difference between the target value and the current number
+            #example if the value at index 0 is 9, and the target=10 then the complement would be 10-9 which is 1
+            complement = target - num
+            #checks if the complement is already present in the seen dictionary, if it is that means we have found a pair of numbers that add up to the target
+            if complement in seen:
+                #in that case, the function returns the index's of the complement and the current number as a list [seen[complement], i]
+                return [seen[complement], i]
+            #if the complement is not in the seen dictionary, it means we haven't encountered the number that pairs with the current number yet. So we add the current number to the seen dictionary (it would be the key) and with its index (it would be the value)
+            seen[num] = i
+        #if the loop finishes without finding a solution, the function returns an empty list indicating there are no 2 numbers that add up to the target value
+        return []
